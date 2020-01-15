000c6d5c9dd9c30f0b4337a6b10905102f10a6ea
JanGin <progchen295@gmail.com>
2020-01-16 00:43:55 +0800

    add test cases

--- src/main/java/craft/calculator/SimpleCalculator.java
+++ src/main/java/craft/calculator/SimpleCalculator.java
@@ -5,14 +5,64 @@ import craft.base.ASTNodeType;
 import craft.base.SimpleASTNode;
 import craft.base.Token;
 import craft.exception.MyParseException;
+import craft.lexer.SimpleLexer;
 import craft.util.TokenReader;
 
 import java.util.Objects;
 
 import static craft.util.TokenUtil.*;
 
+/**
+ *  该class所使用的语法规则：
+ *   additive -> multiplicative | multiplicative + additive
+ *   multiplicative -> primary | primary * multiplicative
+ */
 public class SimpleCalculator {
 
+
+    public static void main(String[] args) throws MyParseException {
+        String script1 = "int a = b + 2;";
+        SimpleLexer lexer = new SimpleLexer();
+        SimpleCalculator cal = new SimpleCalculator();
+        TokenReader tokenReader = lexer.tokenize(script1);
+        /*System.out.println("script1词法解析结果：");
+        lexer.dumpText(tokenReader);
+        tokenReader.setPosition(0);
+        */
+        SimpleASTNode node = cal.intDeclaration(tokenReader);
+        cal.dumpText(node, "");
+        String script2 = "2 + 3 * 6";
+        tokenReader = lexer.tokenize(script2);
+        //System.out.println("script2词法解析结果：");
+        //lexer.dumpText(tokenReader);
+        //tokenReader.setPosition(0);
+        SimpleASTNode node2 = cal.additive(tokenReader);
+        cal.dumpText(node2, "");
+
+        //String script3 = "2 + 3 * 5";         //Result: 17
+        String script3 = "1 + 2 + 3";           //FIXME 此处有结合性问题，先算得5，再算得6
+        tokenReader = lexer.tokenize(script3);
+        SimpleASTNode node3 = cal.additive(tokenReader);
+        cal.calculate(node3, "");
+
+        //以下测试异常状况
+
+        String script4 = "2 +";         //expect right part of the expression after '+' here
+        //tokenReader = lexer.tokenize(script4);
+        //SimpleASTNode node4 = cal.additive(tokenReader);
+        //cal.dumpText(node4, "");
+
+        String script5 = "2 *";       //expect right part of the expression after '*' here
+        //tokenReader = lexer.tokenize(script5);
+        //SimpleASTNode node5 = cal.additive(tokenReader);
+        //cal.dumpText(node5, "");
+
+
+        String script6 = "int a = b + 2";     //Invalid statement, semicolon ';' is excepted
+        //tokenReader = lexer.tokenize(script6);
+        //SimpleASTNode node6 = cal.intDeclaration(tokenReader);
+        //cal.dumpText(node6, "");
+    }
 
     /**
      * 解析基础表达式
@@ -24,8 +74,10 @@ public class SimpleCalculator {
         SimpleASTNode node = null;
         Token token = reader.peek();
         if (isIdentifier(token)) {
+            token = reader.read();
             node = new SimpleASTNode(ASTNodeType.Identifier, token.getTokenText());
         } else if (isIntLiteral(token)) {
+            token = reader.read();
             node = new SimpleASTNode(ASTNodeType.IntLiteral, token.getTokenText());
         } else if (isLeftBracket(token)) {
             token = reader.read();
@@ -56,14 +108,20 @@ public class SimpleCalculator {
         SimpleASTNode node = child1;
         Token token = reader.peek();
         if (isAdditive(token) || isMinus(token)) {
-            token = reader.read();
+            token = reader.read();      //消耗掉这个token
             SimpleASTNode child2 = additive(reader);
             if (Objects.nonNull(child2)) {
                 node = new SimpleASTNode(ASTNodeType.Additive, token.getTokenText());
                 node.addChild(child1);
                 node.addChild(child2);
             } else {
-                throw new MyParseException("Invalid expression, expect right part of the expression here");
+                String symbol = null;
+                if (isAdditive(token))
+                    symbol = "+";
+                else
+                    symbol = "-";
+
+                throw new MyParseException("Invalid expression, expect right part of the expression after '"+ symbol +"' here");
             }
         }
         return node;
@@ -80,22 +138,29 @@ public class SimpleCalculator {
         //
         SimpleASTNode node = child1;
         Token token = reader.peek();
-        if (isMultiple(token) || isDivide(token)) {
-            token = reader.read();
-            SimpleASTNode child2 = primary(reader);
-            if (Objects.nonNull(child2)) {
-                node = new SimpleASTNode(ASTNodeType.Multiplicative, token.getTokenText());
-                node.addChild(child1);
-                node.addChild(child2);
-            } else {
-                throw new MyParseException("Invalid expression, expect right part of the expression here");
+        if (Objects.nonNull(child1) && Objects.nonNull(token)) {
+            if (isMultiple(token) || isDivide(token)) {
+                token = reader.read();
+                SimpleASTNode child2 = primary(reader);
+                if (Objects.nonNull(child2)) {
+                    node = new SimpleASTNode(ASTNodeType.Multiplicative, token.getTokenText());
+                    node.addChild(child1);
+                    node.addChild(child2);
+                } else {
+                    String symbol = null;
+                    if (isMultiple(token))
+                        symbol = "*";
+                    else
+                        symbol = "/";
+                    throw new MyParseException("Invalid expression, expect right part of the expression after '" + symbol + "' here");
+                }
             }
         }
         return node;
     }
 
     /**
-     * 整形表达式
+     * 整型表达式
      * @param reader
      * @return
      */
@@ -139,19 +204,19 @@ public class SimpleCalculator {
      * 计算某个节点的值
      * @return
      */
-    public int calculate(ASTNode node, String text) {
+    public int calculate(ASTNode node, String indent) {
         int result = 0;
         int res1 = 0, res2 = 0;
-        System.out.printf("%s calculating: %s", text, node.getType());
+        System.out.printf("%s calculating: %s", indent, node.getType());
         switch(node.getType()) {
             case Program:
                 for (ASTNode child : node.children()) {
-                    result = calculate(child, text + "\t");
+                    result = calculate(child, indent + "\t");
                 }
                 break;
             case Additive:
-                res1 = calculate(node.children().get(0), text + "\t");
-                res2 = calculate(node.children().get(1), text + "\t");
+                res1 = calculate(node.children().get(0), indent + "\t");
+                res2 = calculate(node.children().get(1), indent + "\t");
                 if ("+".equals(node.getText())) {
                     result = res1 + res2;
                 } else {
@@ -159,8 +224,8 @@ public class SimpleCalculator {
                 }
                 break;
             case Multiplicative:
-                res1 = calculate(node.children().get(0), text + "\t");
-                res2 = calculate(node.children().get(1), text + "\t");
+                res1 = calculate(node.children().get(0), indent + "\t");
+                res2 = calculate(node.children().get(1), indent + "\t");
                 if ("*".equals(node.getText())) {
                     result = res1 * res2;
                 } else {
@@ -173,12 +238,16 @@ public class SimpleCalculator {
             default:
                 break;
         }
-        System.out.println(text + "Result: " + result);
+        System.out.println(indent + "Result: " + result);
         return  result;
     }
 
-    public static void main(String[] args) {
 
+    public void dumpText(ASTNode node, String indent) {
+        System.out.println(indent + node.getType() + ":" + node.getText());
+        for (ASTNode child : node.children()) {
+            dumpText(child, indent + "\t");
+        }
     }
 
 }
